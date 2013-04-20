554fe0549678221c2e3ae6063f4e27e58b6a1d46
Hirokazu NISHIOKA <hiro@nisshiee.org>
2013-04-20 18:08:49 +0900

    [ #5 ] implement Tower types

---
+++ src/test/scala/model/TowerSpec.scala
@@ -0,0 +1,29 @@
+package org.nisshiee.towerdefencescala
+
+import org.specs2._, matcher.DataTables
+
+class TowerSpec extends Specification with DataTables { def is =
+
+  "Tower"                                                                       ^
+    "cost"                                                                      ! e1^
+    "attack"                                                                    ! e2^
+                                                                                end
+
+  def e1 =
+    "type"         | "expected" |
+    WeakTower      !          5 |
+    StrongTower    !        100 |
+    WideRangeTower !         50 |
+    BombTower      !        100 |> { (t, expected) =>
+      t.cost must equalTo(expected)
+    }
+
+  def e2 =
+    "type"         | "expected" |
+    WeakTower      !          1 |
+    StrongTower    !          3 |
+    WideRangeTower !          1 |
+    BombTower      !          1 |> { (t, expected) =>
+      t.attack must equalTo(expected)
+    }
+}
