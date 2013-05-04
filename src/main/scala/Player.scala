424d59a6345ee2b43350c8872b500c533190b2a0
Hirokazu NISHIOKA <hiro@nisshiee.org>
2013-05-04 17:34:20 +0900

    [ #12 ] implement Player class

---
+++ src/main/scala/Player.scala
@@ -0,0 +1,24 @@
+package org.nisshiee.towerdefensescala
+
+import system.{
+   AbstractPlayer => JPlayer
+  ,Command => JCommand
+}
+import java.util.{ List => JList }
+import Conversions._
+
+abstract class Player[A](name: String) extends JPlayer(name) {
+
+  def init: A
+  def action: ((Snapshot, A)) => ((Seq[Command], A))
+
+  private[this] var _stateOpt: Option[A] = None
+
+  override def getCommands: JList[JCommand] = {
+    val snapshot: Snapshot = getGameInfo
+    val state = _stateOpt getOrElse init
+    val (commands, nextState): (Seq[Command], A) = action((snapshot, state))
+    _stateOpt = Some(nextState)
+    commands
+  }
+}
